de30b2882744737049fff9ae5ab07baa773283c1
Daniel Müller <deso@posteo.net>
2023-03-01 15:07:51 -0800

    Test
    See https: // github.com /llvm/ llvm-project/ issues/ 56451

--- setup-build-env/install_clang.sh
+++ setup-build-env/install_clang.sh
@@ -22,6 +22,7 @@ n=0
 while [ $n -lt 5 ]; do
   set +e && \
   wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add - && \
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test && \
   sudo apt-get update && \
   sudo apt-get install -y clang-${LLVM_VERSION} lld-${LLVM_VERSION} llvm-${LLVM_VERSION} && \
   set -e && \
